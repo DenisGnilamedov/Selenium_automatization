9ad05efc98c77ff262dc7bab5f681b892a4e354b
DenisGnilamedov <denisgnilamedov0810@gmail.com>
2021-12-10 13:54:31 +0300

    added tests for login page

--- Part4_PageObject/login_page.py
+++ Part4_PageObject/login_page.py
@@ -9,14 +9,19 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-
         # реализуйте проверку на корректный url адрес
-        assert True
+        assert "login" in self.browser.current_url, 'This is not a login page!'
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
+        self.browser.find_element(*LoginPageLocators.LOGIN_EMAIL), 'Login E-mail is not presented'
+        self.browser.find_element(*LoginPageLocators.LOGIN_PASSWORD), 'Login password is not presented'
         assert True
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_EMAIL), 'Registration E-mail is not presented'
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD), 'Registration password is not presented'
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD_REPEAT), 'Registration password repeat ' \
+                                                                                    'is not presented'
         assert True
