0557f90c049d7c015e477842cec5b70f4621f3b9
DenisGnilamedov <denisgnilamedov0810@gmail.com>
2021-12-10 14:26:16 +0300

    added redirection to login page in test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,6 @@
 from Part4_PageObject.main_page import MainPage
+from Part4_PageObject.login_page import LoginPage
+import time
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -7,10 +9,17 @@ def test_guest_can_go_to_login_page(browser):
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
 
-
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 # pytest -v --tb=line --language=en test_main_page.py
